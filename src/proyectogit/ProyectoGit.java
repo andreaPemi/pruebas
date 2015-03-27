7339989614ad4aecc1ce44f7f38229dd7ef79b7e
Andrea Perez <anmapemi06@hotmail.com>
2015-03-27 21:21:55 +0100

    carga inical del proyecto

---
+++ src/proyectogit/ProyectoGit.java
@@ -0,0 +1,16 @@
+
+
+package proyectogit;
+
+public class ProyectoGit {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Pruebando remoto remoto");
+
+
+
+    }
+
+}
